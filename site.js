9bc06df4540df2c623a86d03f0a41d781faa6c40
Qi Zhang <zhangqitydx@gmail.com>
2017-09-06 18:15:35 -0500

    Update recent changes

--- site.js
+++ site.js
@@ -7,12 +7,30 @@ function howManyPets(user) {
   return user.name + " has " + user.pets.length + " pet" + plural;
 }
 
-var user = {
-  name:"QI",
-  age: 22,
-  pets: ["lili","lola"],
-  count_pets: function() {
-    return this.pets.length;
-  }
-  num_pets:  
-}
+//var user = {
+//  name:"QI",
+//  age: 22,
+//  pets: ["lili","lola"],
+//  count_pets: function() {
+//    return this.pets.length;
+//  }
+//  num_pets:
+//}
+
+//$('#user-form').on('submit', function(e) {
+  //var username = $('#username').val();
+  //console.log(username);
+  //e.preventDefault();
+//});
+
+// $('#user-form').on('submit', function(e) {
+//   var username = $('#username').val();
+//   $('#results').append('<li>'+ username +'</li>');
+//   e.preventDefault();
+// })
+
+$('#user-form').on('submit', function(e) {
+  var username = $('#username').val();
+  $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' + username + '</a></li>');
+  e.preventDefault();
+});
