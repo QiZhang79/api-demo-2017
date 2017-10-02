3ce82a83773abfc3f52e2edcdc1cc4fed4156c15
Qi Zhang <zhangqitydx@gmail.com>
2017-10-02 14:25:07 -0500

    class review and add some comments

--- ajax_practice/site.js
+++ ajax_practice/site.js
@@ -1,11 +1,22 @@
-var my_data;
+var my_data = [];
 $('#user-form').on('submit', function(e) {
   var username = $('#username').val();
   var url = 'https://api.github.com/users/' + username;
   $.get(url, function(data) {
-    my_data = data;
-    console.log("Your data should be ready in `my_data`");
-  });
+    var name = data.name;
+    var img = data.avatar_url;
+    if (data.name == null) {
+        name = data.login;
+    }
+    $("#results").append('<li><a id = "'+ data.login +'" href = "'+ data.html_url +
+      '">' + name + '</a></li>'); //Be aware of Concatinate
+    $('#' + data.login).prepend('<img src="' + img + '" />');
+    // The .prepend() method inserts the specified content as the first child of
+    //each element in the jQuery collection (To insert it as the last child, use
+    //.append()).
 
+
+  });
   e.preventDefault();
+
 });
